06e82228a6e80347aa335befbd19a7b6d97b3d22
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2024-05-29 10:47:21 +0200

    Switched to new input system (very high look sensitivity?)

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
     public bool useCharacterForward = false;
     public bool lockToCameraForward = false;
     public float turnSpeed = 10f;
-    public float speed = 20f; 
+    public float speed = 20f;
 
     private float turnSpeedMultiplier;
     private float direction = 0f;
@@ -13,45 +14,49 @@ public class CharacterMovement : MonoBehaviour
     private Vector2 input;
     private Quaternion freeRotation;
     private Camera mainCamera;
+    private Rigidbody rb;
 
-    // Use this for initialization
     void Start()
     {
         mainCamera = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    void OnMove(InputValue movementValue)
+    {
+        input = movementValue.Get<Vector2>();
+    }
+
+    void OnLook(InputValue lookValue)
+    {
+        // ???
+    }
+
     void FixedUpdate()
     {
-        input.x = Input.GetAxis("Horizontal");
-        input.y = Input.GetAxis("Vertical");
+        // Calculate direction based on input
+        direction = input.magnitude;
 
-        // Calculate direction and speed based on input
-        if (useCharacterForward)
-            direction = Mathf.Abs(input.x) + input.y;
-        else
-            direction = Mathf.Abs(input.x) + Mathf.Abs(input.y);
-
-        direction = Mathf.Clamp(direction, 0f, 1f);
-
-        // Update target direction relative to the camera view (or not if the Keep Direction option is checked)
+        // Update target direction relative to the camera view
         UpdateTargetDirection();
+
         if (input != Vector2.zero && targetDirection.magnitude > 0.1f)
         {
             Vector3 lookDirection = targetDirection.normalized;
             freeRotation = Quaternion.LookRotation(lookDirection, transform.up);
-            var differenceRotation = freeRotation.eulerAngles.y - transform.eulerAngles.y;
-            var eulerY = transform.eulerAngles.y;
+            float differenceRotation = freeRotation.eulerAngles.y - transform.eulerAngles.y;
+            float eulerY = transform.eulerAngles.y;
 
-            if (differenceRotation < 0 || differenceRotation > 0) eulerY = freeRotation.eulerAngles.y;
+            if (differenceRotation < 0 || differenceRotation > 0)
+                eulerY = freeRotation.eulerAngles.y;
+
             var euler = new Vector3(0, eulerY, 0);
-
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(euler), turnSpeed * turnSpeedMultiplier * Time.deltaTime);
         }
 
         // Move the character
-        Vector3 movement = targetDirection * speed * direction * Time.deltaTime;
-        transform.position += movement;
+        Vector3 movement = targetDirection * speed * direction * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
     }
 
     public virtual void UpdateTargetDirection()
@@ -59,23 +64,23 @@ public class CharacterMovement : MonoBehaviour
         if (!useCharacterForward)
         {
             turnSpeedMultiplier = 1f;
-            var forward = mainCamera.transform.TransformDirection(Vector3.forward);
+            Vector3 forward = mainCamera.transform.TransformDirection(Vector3.forward);
             forward.y = 0;
 
-            // get the right-facing direction of the referenceTransform
-            var right = mainCamera.transform.TransformDirection(Vector3.right);
+            // Get the right-facing direction of the referenceTransform
+            Vector3 right = mainCamera.transform.TransformDirection(Vector3.right);
 
-            // determine the direction the player will face based on input and the referenceTransform's right and forward directions
+            // Determine the direction the player will face based on input and the referenceTransform's right and forward directions
             targetDirection = input.x * right + input.y * forward;
         }
         else
         {
             turnSpeedMultiplier = 0.2f;
-            var forward = transform.TransformDirection(Vector3.forward);
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
             forward.y = 0;
 
-            // get the right-facing direction of the referenceTransform
-            var right = transform.TransformDirection(Vector3.right);
+            // Get the right-facing direction of the referenceTransform
+            Vector3 right = transform.TransformDirection(Vector3.right);
             targetDirection = input.x * right + Mathf.Abs(input.y) * forward;
         }
     }
